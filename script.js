9c08f2b827b5ad24cce12ec253ad29dbdd6189b8
Masagus <agusyantosugiyanto@gmail.com>
2024-06-02 19:43:44 +0700

    Add Extraculliculars Javavscript

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ function changePoster(n) {
     showPoster(slideIndex += n)
 }
 function showPoster(n) {
-    var i;
-    var x = document.getElementsByClassName("posters");
-    var y = document.getElementsByClassName("posters-info")
+    let i;
+    let x = document.getElementsByClassName("posters");
+    let y = document.getElementsByClassName("posters-info")
     if (n > x.length) {
         slideIndex = 1
     }
@@ -28,3 +28,33 @@ function showPoster(n) {
     x[slideIndex-1].style.display = "block";
     y[slideIndex-1].style.display = "block"
 }
+
+var excull_name = ["Band", "Basket", "English Club", 
+                "Futsal", "Jappannes Club", "KIR", 
+                "Merpati Putih", "Modern Dance", "Pramuka",
+                "Student Company", "Rohis/Rokris", "Shaman",
+                "Taekwondo", "Tari Tradisional", "Tennis",
+                "TIGMAPALA", "Vocal Group", "Voli"
+];
+var excull_desc = [];
+
+function excull() {
+    let i;
+
+    let parent = document.getElementById("excull-container");
+    
+    for (i=0; i < excull_name.length; i++) {
+        let div = document.createElement("div");
+        let h3 = document.createElement("h3");
+        let p = document.createElement("p");
+        
+        h3.innerHTML = i+1 + ". " + excull_name[i];
+        p.innerHTML = "lorem Ipsum loremipsum lorem ipsum lorem ipseum"
+
+        parent.appendChild(div);
+        div.appendChild(h3);
+        div.appendChild(p)
+    }
+
+}
+excull()
